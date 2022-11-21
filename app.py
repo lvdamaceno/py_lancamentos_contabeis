ce090e8af87071ea1c88db9de26a63664926623a
lvdamaceno <lvdamaceno@gmail.com>
2022-11-20 22:21:42 -0300

    Correção nas rotas

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from werkzeug.utils import secure_filename
 from datetime import datetime
 from script import generate_xml
 
+
+app = Flask(__name__)
 ALLOWED_EXTENSIONS = {'csv'}
 
 
@@ -12,33 +14,32 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def create_app():
-    app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            new_filename = f'{filename.split(".")[0]}_{str(datetime.now())}.csv'
+            save_location = os.path.join('inputs', new_filename)
+            file.save(save_location)
+            output_file = generate_xml(save_location)
+            # return send_from_directory('outputs', output_file)
 
-    @app.route('/upload', methods=['GET', 'POST'])
-    def upload():
-        if request.method == 'POST':
-            file = request.files['file']
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                new_filename = f'{filename.split(".")[0]}_{str(datetime.now())}.csv'
-                save_location = os.path.join('inputs', new_filename)
-                file.save(save_location)
+        return redirect(url_for('download'))
 
-                output_file = generate_xml(save_location)
-                # return send_from_directory('outputs', output_file)
-
-            return redirect(url_for('download'))
-
-        return render_template('upload.html')
-
-    @app.route('/download')
-    def download():
-        return render_template('download.html', files=os.listdir('outputs'))
+    return render_template('index.html')
 
 
-    @app.route('/download/<filename>')
-    def download_file(filename):
-        return send_from_directory('outputs', filename)
+@app.route('/download')
+def download():
+    return render_template('download.html', files=os.listdir('outputs'))
 
-    return app
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory('outputs', filename)
+
+
+if __name__ == "__main__":
+    app.run()
